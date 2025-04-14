af29baf11e4847d3c8898018c90077bc318a7642
Art4 <art4@wlabs.de>
2025-04-14 14:36:46 +0000

    deprecate value `monolog` for config `system.logger_config`

--- src/Core/Logger/Factory/DelegatingLoggerFactory.php
+++ src/Core/Logger/Factory/DelegatingLoggerFactory.php
@@ -45,6 +45,15 @@ final class DelegatingLoggerFactory implements LoggerFactory
 	{
 		$factoryName = $this->config->get('system', 'logger_config') ?? '';
 
+		/**
+		 * @deprecated 2025.02 The value `monolog` for `system.logger_config` inside the `config/local.config.php` file is deprecated, please use `stream` or `syslog` instead.
+		 */
+		if ($factoryName === 'monolog') {
+			@trigger_error('The config `system.logger_config` with value `monolog` is deprecated since 2025.02 and will stop working in 5 months, please change the value to `stream` or `syslog` in the `config/local.config.php` file.', \E_USER_DEPRECATED);
+
+			$factoryName = 'stream';
+		}
+
 		if (!array_key_exists($factoryName, $this->factories)) {
 			return new NullLogger();
 		}
