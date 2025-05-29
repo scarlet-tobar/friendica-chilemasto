3def76649b38b31dfc9f5254e593142852abc024
Philipp <admin@philipp.info>
2025-05-29 19:06:13 +0200

    Fix PHP-CS

--- src/Module/Contact.php
+++ src/Module/Contact.php
@@ -256,7 +256,7 @@ class Contact extends BaseModule
 			$search_txt = preg_quote(trim($search, ' @!'));
 			$sql_extra .= " AND (CAST(`name` AS BINARY) REGEXP BINARY ? OR CAST(`url` AS BINARY) REGEXP BINARY ?";
 			$sql_extra .= " OR  CAST(`nick` AS BINARY) REGEXP BINARY ? OR  CAST(`addr` AS BINARY) REGEXP BINARY ?";
-                        $sql_extra .= " OR  CAST(`alias` AS BINARY) REGEXP BINARY ?)";
+			$sql_extra .= " OR  CAST(`alias` AS BINARY) REGEXP BINARY ?)";
 			$sql_values[] = $search_txt;
 			$sql_values[] = $search_txt;
 			$sql_values[] = $search_txt;
