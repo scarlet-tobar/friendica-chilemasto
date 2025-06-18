179c4dc78d2595fa7b4f3951ecda857edbbadf4a
Art4 <art4@wlabs.de>
2025-06-18 09:19:45 +0000

    Let WorkerLogger implement new DefaultContextLogger

---
+++ src/Core/Logger/Capability/DefaultContextLogger.php
@@ -0,0 +1,21 @@
+<?php
+
+// Copyright (C) 2010-2024, the Friendica project
+// SPDX-FileCopyrightText: 2010-2024 the Friendica project
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare(strict_types=1);
+
+namespace Friendica\Core\Logger\Capability;
+
+/**
+ * Add a default context to the logger that will be used for every log entry.
+ */
+interface DefaultContextLogger
+{
+	/**
+     * @return array The old context that will be replaced with the new context
+     */
+    public function replaceDefaultContext(array $defaultContext): array;
+}
