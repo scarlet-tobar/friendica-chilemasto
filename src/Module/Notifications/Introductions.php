3386e42962adcc06e9c756733bb03b33d7e1e00d
Marcus Funch <mfm@magenta.dk>
2025-09-24 15:02:46 +0200

    Terminology: Subscriber -> Follower

--- src/Module/Notifications/Introductions.php
+++ src/Module/Notifications/Introductions.php
@@ -80,7 +80,7 @@ class Introductions extends BaseNotifications
 
 		$notificationResult = $this->getNotifications();
 		$notifications      = $notificationResult['notifications'] ?? [];
-		$notificationHeader = $notificationResult['header'] ?? '';
+		$notificationHeader = $notificationResult['header']        ?? '';
 
 		$notificationSuggestions = Renderer::getMarkupTemplate('notifications/suggestions.tpl');
 		$notificationTemplate    = Renderer::getMarkupTemplate('notifications/intros.tpl');
@@ -129,24 +129,24 @@ class Introductions extends BaseNotifications
 					]);
 					break;
 
-				// Normal connection requests
+					// Normal connection requests
 				default:
 					if ($Introduction->getNetwork() === Protocol::DFRN) {
 						$lbl_knowyou = $this->t('Claims to be known to you: ');
 						$knowyou     = ($Introduction->getKnowYou() ? $this->t('Yes') : $this->t('No'));
 					} else {
 						$lbl_knowyou = '';
-						$knowyou = '';
+						$knowyou     = '';
 					}
 
 					$convertedName = BBCode::toPlaintext($Introduction->getName(), false);
 
-					$helptext  = $this->t('Shall your connection be bidirectional or not?');
-					$helptext2 = $this->t('Accepting %s as a friend allows %s to subscribe to your posts, and you will also receive updates from them in your news feed.', $convertedName, $convertedName);
-					$helptext3 = $this->t('Accepting %s as a subscriber allows them to subscribe to your posts, but you will not receive updates from them in your news feed.', $convertedName);
+					$helptext  = $this->t('Accept %s as a friend or follower?', $convertedName);
+					$helptext2 = $this->t('Accepting %s as a friend allows them to follow your posts, and you will also receive updates from them in your news feed.', $convertedName);
+					$helptext3 = $this->t('Accepting %s as a follower allows them to follow your posts, but you will not receive updates from them in your news feed.', $convertedName);
 
-					$friend = ['duplex', $this->t('Friend'), '1', $helptext2, true];
-					$follower = ['duplex', $this->t('Subscriber'), '0', $helptext3, false];
+					$friend   = ['duplex', $this->t('Friend (Follow them back)'), '1', $helptext2, true];
+					$follower = ['duplex', $this->t('Follower'), '0', $helptext3, false];
 
 					$action = 'follow_confirm';
 
