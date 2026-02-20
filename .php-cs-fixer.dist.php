ff1b44ffde336415721f948d27578478dfdd4f77
Art4 <art4@wlabs.de>
2026-02-20 10:56:34 +0000

    chore: add the php-cs-fixer.dist.php itself and let it fix the code style

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * SPDX-FileCopyrightText: 2010 - 2024 the Friendica project
  *
@@ -11,24 +12,30 @@ require_once __DIR__ . '/bin/dev/php-cs-fixer/vendor/autoload.php';
 
 $finder = PhpCsFixer\Finder::create()
 	->in(__DIR__)
-	->notPath('addon')
-	->notPath('bin/dev')
-	->notPath('config')
-	->notPath('doc')
-	->notPath('images')
-	->notPath('mods')
-	->notPath('spec')
-	->notPath('vendor')
-	->notPath('view/asset')
-	->notPath('lang')
-	->notPath('view/smarty3/compiled');
+	->exclude([
+		'addon',
+		'bin/dev',
+		'config',
+		'doc',
+		'images',
+		'mods',
+		'spec',
+		'vendor',
+		'view/asset',
+		'lang',
+		'view/smarty3/compiled',
+	])
+	->append([
+		'.php-cs-fixer.dist.php',
+	])
+;
 
 $config = new PhpCsFixer\Config();
 return $config
 	->setRules([
 		'@PER-CS3x0'              => true,
 		'align_multiline_comment' => true,
-		'binary_operator_spaces' => [
+		'binary_operator_spaces'  => [
 			'default'   => 'single_space',
 			'operators' => [
 				'=>' => 'align_single_space_minimal',
@@ -36,7 +43,7 @@ return $config
 				'??' => 'align_single_space_minimal',
 			],
 		],
-		'braces_position'        => [
+		'braces_position' => [
 			'anonymous_classes_opening_brace'  => 'same_line',
 			'control_structures_opening_brace' => 'same_line',
 			'functions_opening_brace'          => 'next_line_unless_newline_at_signature_end',
@@ -44,13 +51,13 @@ return $config
 		'function_declaration' => [
 			'closure_function_spacing' => 'one',
 		],
-		'list_syntax'      => [
+		'list_syntax' => [
 			'syntax' => 'short',
 		],
-		'no_unused_imports'                  => true,
-		'single_import_per_statement'        => true,
-		'ternary_operator_spaces'            => false,
-		'new_with_parentheses' => true,
+		'no_unused_imports'           => true,
+		'single_import_per_statement' => true,
+		'ternary_operator_spaces'     => false,
+		'new_with_parentheses'        => true,
 	])
 	->setFinder($finder)
 	->setIndent("\t");
